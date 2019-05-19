affab5601d3c5791e26fb3ac8419751368d5c221
MilesTin <1154520245@qq.com>
2019-05-19 23:57:36 +0800

    URL CHANGE

--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,9 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-    path("login/", login, name="account"),
-    path("logout/", logout, name="logout"),
+    path("login", login, name="account"),
+    path("logout", logout, name="logout"),
     path("getCaptcha",getCaptcha,name="getCaptch"),#获得验证码
     path("verifStuId",verifStuId,name="verifStuId"),#绑定学号,学号验证
-
+    path("myOrder",myorder,name="myOrder"),#我的订单
 ]
